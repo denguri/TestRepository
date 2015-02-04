011befc81b90365991356ed26005736218e8c4b1
denguri <denguri@dengurizaemon>
2015-02-05 00:11:51 +0100

    first commit

---
+++ testGame/android/src/com/majstor/jovan/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.majstor.jovan.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.majstor.jovan.TestGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TestGame(), config);
+	}
+}
---
+++ testGame/desktop/src/com/majstor/jovan/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.majstor.jovan.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.majstor.jovan.TestGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TestGame(), config);
+	}
+}
