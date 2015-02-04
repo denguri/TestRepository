b412df74c5abacf1996d59d894401c7964d9e5ac
denguri <denguri@dengurizaemon>
2015-02-05 00:19:15 +0100

    xxx

--- testGame/desktop/src/com/majstor/jovan/desktop/DesktopLauncher.java
+++ testGame/desktop/src/com/majstor/jovan/desktop/DesktopLauncher.java
@@ -3,7 +3,8 @@ package com.majstor.jovan.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.majstor.jovan.TestGame;
-
+//sdasd
+//asdasdasd
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
